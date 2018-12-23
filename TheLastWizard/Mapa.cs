85402d4f5e4601503d05e224600779b587405266
carroyoES <40261086+carroyoES@users.noreply.github.com>
2018-12-23 22:42:26 +0100

    cofre y apuntes hechizos
    cofre y apuntes hechizos

--- TheLastWizard/Mapa.cs
+++ TheLastWizard/Mapa.cs
@@ -18,7 +18,7 @@ namespace TheLastWizard {
             this.juego = juego;
             matrizMapa = new Zona[,] {
                 { new Zona("Estás en la entrada\n", "", false, null, false, true, false, false), new Zona("Estás en un pasillo.\n", "Un goblin te sale al encuentro.\n", true, new ElementoEntorno[]{new Goblin(juego, new Item[] { new Llave(juego, "LLAVE COCINA") }), new Puerta(juego, true, true, "LLAVE COCINA", "SUR") }, false, false, false, true) },
-                { new Zona("Estás en un viejo dormitorio\n", "", false, null, false, true, false, false), new Zona("Estás en lo que parece ser la cocina\n", "", false, null, true, false, false, true)},
+                { new Zona("Estás en un viejo dormitorio\n", "", false, new ElementoEntorno[]{new Cofre(juego)}, false, true, false, false), new Zona("Estás en lo que parece ser la cocina\n", "", false, null, true, false, false, true)},
             };
         }
 
---
+++ TheLastWizard/Objetos/Muebles/Cofre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLastWizard.Objetos.Muebles {
+    public class Cofre : Mueble {
+        public Cofre(Juego juego) : base(juego) {
+            this.nombre = "Cofre";
+            this.boton.Text = nombre;
+            this.sePuedeAbrir = true;
+        }
+    }
+}
